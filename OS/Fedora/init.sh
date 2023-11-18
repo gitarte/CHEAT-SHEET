b6abae8b4abf5aa538582f04f7b49ef6ba5d3291
Artur Gawroński <art-e@o2.pl>
2023-11-18 17:55:01 +0100

    Update for Fedora 39

--- OS/Fedora/init.sh
+++ OS/Fedora/init.sh
@@ -1,30 +1,27 @@
-fastestmirror=true
-max_parallel_downloads=20
-deltarpm=1
-ip_resolve=4
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo "fastestmirror=True"        >> /etc/dnf/dnf.conf
+echo "deltarpm=1"                >> /etc/dnf/dnf.conf
+echo "ip_resolve=4"              >> /etc/dnf/dnf.conf
 
+&& sudo dnf -y update \
+&& sudo dnf -y upgrade \
+&& sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+&& sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+&& sudo dnf -y upgrade --refresh \
+&& sudo dnf -y groupupdate core \
+&& sudo dnf -y check \
+&& sudo dnf -y autoremove \
 && sudo fwupdmgr get-devices \
 && sudo fwupdmgr refresh --force \
 && sudo fwupdmgr get-updates \
 && sudo fwupdmgr update \
-
-&& sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-&& sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
-&& sudo dnf -y upgrade --refresh \
-&& sudo dnf -y check \
-&& sudo dnf -y autoremove \
-&& sudo dnf -y install tlp tlp-rdw \
-&& sudo systemctl enable tlp \
-&& sudo dnf -y install gnome-extensions-app gnome-tweaks gnome-shell-extension-window-list gnome-shell-extension-appindicator gnome-shell-extension-apps-menu gnome-shell-extension-dash-to-dock \
-&& sudo dnf -y install curl cabextract xorg-x11-font-utils fontconfig \
 && sudo dnf -y groupupdate sound-and-video \
-&& sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg \
 && sudo dnf -y install lame\* --exclude=lame-devel \
-&& sudo dnf -y group upgrade --with-optional Multimedia \
-&& sudo dnf -y install vlc mplayer audacity \
-&& sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc \
-&& sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' \
-&& sudo dnf -y check-update \
-&& sudo dnf -y install code \
-&& sudo dnf -y install gparted \
+&& sudo dnf -y install tlp tlp-rdw && sudo systemctl enable tlp \
+&& sudo dnf -y install vim vlc gimp gnome-tweaks unzip p7zip p7zip-plugins unrar vlc mplayer audacity gparted \
+&& sudo dnf -y install kernel-devel.x86_64 qt5-qttools-devel.x86_64 qt5-qttools-libs-help.x86_64 libvpx7.x86_64 \
+&& sudo dnf -y install dnf-plugins-core \
+&& sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo \
+&& sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc \
+&& sudo dnf -y install brave-browser \
 && echo "all done"
